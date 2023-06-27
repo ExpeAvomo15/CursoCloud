c96fa35d71dbccce099124962f722799f94769eb
Expe Avomo <soyexpeavomo@gmail.com>
2023-06-27 14:44:11 +0200

    App Restaurante Maven 27/06/2023
    Actualizaciones del proyectoi

--- restaurantesmalaga/src/main/java/edu/arelance/nube/controller/RestauranteController.java
+++ restaurantesmalaga/src/main/java/edu/arelance/nube/controller/RestauranteController.java
@@ -1,11 +1,18 @@
 package edu.arelance.nube.controller;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.arelance.nube.repository.entity.Restaurante;
+import edu.arelance.nube.service.RestauranteService;
 
 /**
  * 
@@ -25,14 +32,44 @@ import edu.arelance.nube.repository.entity.Restaurante;
 
 @RequestMapping("/restaurante")
 public class RestauranteController {
+	@Autowired
+	RestauranteService restuaranteService;
 
 	@GetMapping("/test")
 	public Restaurante obtenerRestauranteTest() {
 		Restaurante restaurante = null;
 		
 		System.out.println("llamando a obtenerRestauranteTest");
+		restaurante = new Restaurante(1l, "Martinete", "Carlos Haya 33","Carranque", "www.martinete.org", "https://google.xe", 33.65f, -2.3f, 10, "Gazpacheuelo", "paella","sopa de marisco",LocalDateTime.now());
 		
 		return restaurante;
 		
 	}
+	
+	// GET -> Consultar TODOS GET http://localhost:8081/restaurante/test
+    @GetMapping
+	public ResponseEntity<?> obtenerListaRestaurantes (){
+	 ResponseEntity<?>  responseEntity = null;
+	 //ResponseEntity<?> representa un mensaje http
+	 Iterable<Restaurante> lista_Restaurantes = null;
+	lista_Restaurantes = this.restuaranteService.consultarTodos();
+     responseEntity =	ResponseEntity.ok(lista_Restaurantes);
+	 return responseEntity;
+ }
+	// GET -> Consultar uno (por ID)
+    @GetMapping("/id")
+	public ResponseEntity<?> ListarID (@PathVariable Long id){
+    	ResponseEntity<?> responseEntity = null;
+    	
+    	return responseEntity;
+    }
+    
+ // DELETE -> Consultar uno (por ID)  http://localhost:8081/restaurante/test
+    @DeleteMapping("/id")
+	public ResponseEntity<?> borrarPorID (@PathVariable Long id){
+    	ResponseEntity<?> responseEntity = null;
+    	
+    	return responseEntity;
+    }
+	
 }
--- restaurantesmalaga/src/main/java/edu/arelance/nube/service/RestauranteServiceImpl.java
+++ restaurantesmalaga/src/main/java/edu/arelance/nube/service/RestauranteServiceImpl.java
@@ -1,9 +1,57 @@
 package edu.arelance.nube.service;
 
+import java.util.Optional;
+
+//import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.arelance.nube.repository.RestauranteRepository;
+import edu.arelance.nube.repository.entity.Restaurante;
 
 @Service
 
 public class RestauranteServiceImpl implements RestauranteService {
 
+	RestauranteRepository restauranteRepository;
+	@Override
+	@Transactional(readOnly = true)//permitimos acceso concurrente a la tabla Restaurantes
+	public Iterable<Restaurante> consultarTodos() {
+		// TODO Auto-generated method stub
+		return this.restauranteRepository.findAll();
+		//return null;
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Optional<Restaurante> consultarRestaurante(Long id) {
+		// TODO Auto-generated method stub
+		return this.restauranteRepository.findById(id);
+		//return null;
+	}
+
+	@Override
+	@Transactional
+	
+	public Restaurante altaRestaurante(Restaurante restaurante) {
+		// TODO Auto-generated method stub
+		return this.restauranteRepository.save(restaurante);
+		//return null;
+	}
+
+	@Override
+	@Transactional
+	public void borrarRestaurante(Long id) {
+		// TODO Auto-generated method stub
+		this.restauranteRepository.deleteById(id);
+	}
+
+	@Override
+	public Optional<Restaurante> modificarRestaurante(Long id, Restaurante restaurante) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 }
